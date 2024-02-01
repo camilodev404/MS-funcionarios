592085de37b3ab1b9bd39d1fc16e6e7833d2657c
CamiloDev404 <crosti1826@gmail.com>
2024-01-31 23:49:48 -0500

    fist commit to the project

---
+++ src/main/java/com/funcionarios/FuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.funcionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funcionarios/FuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funcionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
