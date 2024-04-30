6ee815194a8d6c58a8721ce3c2ca06576dbec305
CamiloDev404 <crosti1826@gmail.com>
2024-04-29 22:00:32 -0500

    Documentacion del microservicio

--- src/main/java/com/funcionarios/Controller/FuncionarioController.java
+++ src/main/java/com/funcionarios/Controller/FuncionarioController.java
@@ -2,6 +2,7 @@ package com.funcionarios.Controller;
 
 import com.funcionarios.Model.Funcionario;
 import com.funcionarios.Service.FuncionarioService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/funcionario")
 @CrossOrigin("*")
+@Tag(name = "Funcionarios")
 public class FuncionarioController {
 
     @Autowired
--- src/main/java/com/funcionarios/Controller/UgtController.java
+++ src/main/java/com/funcionarios/Controller/UgtController.java
@@ -2,6 +2,7 @@ package com.funcionarios.Controller;
 
 import com.funcionarios.Model.Ugt;
 import com.funcionarios.Service.UgtService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/ugt")
 @CrossOrigin("*")
+@Tag(name = "Ugts")
 public class UgtController {
 
     @Autowired
